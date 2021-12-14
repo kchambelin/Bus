e72a342d061e3750bb9308f33cb64fcfda86d4c3
Guitare_ <kevin.chambelin@orange.fr>
2021-12-14 09:22:13 +0100

    page_creation

---
+++ resources/views/manage_bus.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+{{-- @section('title', 'Tableau de bord')
+@section('header.title', 'Partenaires Particuliers')
+@section('breadcumb.first.title', 'ACCUEIL')
+@section('breadcumb.second.title', 'PRESENTATION') --}}
+
+@section('page.content')
+
+    <!--begin::Entry-->
+    <div class="d-flex flex-column-fluid">
+        <!--begin::Container-->
+        <div class="container">
+
+        </div>
+        <!--end::Container-->
+    </div>
+    <!--end::Entry-->
+
+@endsection
+
+@section('page.script')
+
+@endsection
